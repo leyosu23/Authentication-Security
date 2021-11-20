e4daaa2ebb7141766b580760dbe92522eccee3c3
leyosu23 <leyosu23@gmail.com>
2021-11-20 17:36:30 -0500

    Register users with userDB using mongoDB (Level 1 security)

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
 
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 
@@ -12,6 +15,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+// user Model
+const User = new mongoose.model("User", userSchema)
 
 app.get("/", function(req, res){
     res.render("home");
@@ -23,6 +36,36 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
     res.render("register");
+})
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    })
+})
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                if ( foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
 })
 
 app.listen(3000, function() {
